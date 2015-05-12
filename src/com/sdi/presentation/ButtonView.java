7209782052516bce0f88e7036fe055d8ede423c8
pablobravo <pablobravomediavilla@gmail.com>
2015-05-12 13:21:02 +0200

    Añadidas clases complementarias para que funcione el asunto

---
+++ src/com/sdi/presentation/ButtonView.java
@@ -0,0 +1,21 @@
+package com.sdi.presentation;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+@ManagedBean(name="buttonView")
+@SessionScoped
+public class ButtonView {
+     
+    public void buttonAction(ActionEvent actionEvent) {
+        addMessage("Welcome to Primefaces!!");
+    }
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
